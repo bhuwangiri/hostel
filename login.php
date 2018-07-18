aa0d18e333236b31201907acbf2017e5a32025b1
bhuwangiri <bhuwangiri1997@gmail.com>
2018-07-18 09:28:21 +0545

    adding initial files to hostel management system

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTTYPE HTML>
+<?php
+ $email="email";
+ $password="password";
+     if (isset() {
+ }
+    ?>
+    <html>
+      <body>
+         <form method="post" action="index.php">
+         username:<br/>
+         <input type="text" name="username"><br/>
+         password<br/>
+         <input type="password" name="password"><br/>
+         password<br/>
+         <input type="submit" name="submit" value="login!">
+</form>
+</body>
+</html>
